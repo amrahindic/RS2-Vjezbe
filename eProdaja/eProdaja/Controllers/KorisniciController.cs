fe946310b8eb52b5a81070d45a81f5b39209dcfa
amrahindic <amrahindiic@gmail.com>
2022-02-12 02:02:20 +0100

    V2
    Vježba 2 iz predmeta Razvoj Softvera II

---
+++ eProdaja/eProdaja/Controllers/KorisniciController.cs
@@ -0,0 +1,32 @@
+﻿using eProdaja.Database;
+using eProdaja.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eProdaja.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KorisniciController : ControllerBase
+    {
+        public IKorisniciService _korisnikService;
+
+        public KorisniciController(IKorisniciService korisnikService)
+        {
+            _korisnikService = korisnikService;
+        }
+
+        [HttpGet]
+        public List<Model.Korisnici> Get()
+        {
+
+
+            return _korisnikService.GET();
+        }
+
+
+    }
+}
---
+++ eProdaja/eProdaja/Services/KorisniciService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using eProdaja.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public class KorisniciService : IKorisniciService
+    {
+        public eProdajaContext Context { get; set; }
+        protected readonly IMapper _mapper;
+        public KorisniciService(eProdajaContext context, IMapper mapper)
+        {
+            Context = context;
+            _mapper = mapper;
+        }
+        public List<Model.Korisnici> GET()
+        {
+            return Context.Korisnicis.ToList().Select(x => _mapper.Map<Model.Korisnici>(x)).ToList();
+        }
+
+     /*   private Model.Korisnici ToModel(Korisnici entity)
+        {
+            return new Model.Korisnici()
+            {
+                KorisnikId = entity.KorisnikId,
+                KorisnickoIme = entity.KorisnickoIme,
+                Ime = entity.Ime,
+                Prezime = entity.Prezime,
+                Email = entity.Email,
+                Telefon = entity.Telefon
+            };
+        }
+     */
+    }
+}
